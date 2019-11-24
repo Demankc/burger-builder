3009879921f77e12ebb160b45cf74d78eea86138
Manoj Chowdhury <manojc009@gmail.com>
2019-11-24 02:13:37 +0100

    removing api key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,7 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
-const apiKey = 'AIzaSyDvTM-Y9wPPC69vaUp1jEfqhO13HV7iguY';
+const apiKey = '';
 export const authStart = () => {
     return {
         type: actionTypes.AUTH_START
